58c6f4e18fbfb1a97c6d79f0d7809e3eec2ee368
flagar0 <flaben10@gmail.com>
2023-06-17 15:53:57 -0300

    inicio transicao opencvsharp

--- ArTracking/Form1.cs
+++ ArTracking/Form1.cs
@@ -12,6 +12,9 @@ using Emgu.CV.Aruco;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
+using OpenCvSharp;
+using OpenCvSharp.Aruco;
+using OpenCvSharp.Internal.Vectors;
 
 namespace ArTracking
 {
@@ -22,6 +25,7 @@ namespace ArTracking
         DetectorParameters ArucoParameters;
         public bool recording = false;
         Mat cameraMatrix, distortionMatrix;
+
         Dictionary<string,string> Dados = new Dictionary<string, string>() {
             {"translation_x", ""},
             {"translation_y", ""},
@@ -29,6 +33,8 @@ namespace ArTracking
 
 
         };
+
+
 
         public Form1()
         {
@@ -60,12 +66,12 @@ namespace ArTracking
             #region Initialize video capture object on default webcam (0)
             // Instantiate a webcam abstraction
 
-            capture = new VideoCapture(0);
+            capture = new VideoCapture(0); 
 
-            ArucoDict = new Dictionary(Dictionary.PredefinedDictionaryName.Dict6X6_250);
+             ArucoDict = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.Dict6X6_250);
 
             ArucoParameters = new DetectorParameters();
-            ArucoParameters = DetectorParameters.GetDefault();
+            ArucoParameters = DetectorParameters.
 
 
             Calibracao();
@@ -79,12 +85,22 @@ namespace ArTracking
             Tracking();
         }
 
-
+        /// <summary>
+        /// Convert a rotation vector in a rotation matrix using Rodrigues algorithm.
+        /// </summary>
+        /// <param name="rvec">The rotation vector to convert.</param>
+        /// <returns></returns>
+        Mat GetRotationMatrixFromRotationVector(Mat rvec)
+        {
+            Mat rmat = new Mat();
+            Cv2.Rodrigues(rvec, rmat);
+            return rmat;
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
             recording = false;
-            CvInvoke.DestroyAllWindows();
+            Cv2.DestroyAllWindows();
         }
 
         public void Tracking()
@@ -94,31 +110,31 @@ namespace ArTracking
 
                 #region Capture a frame with webcam
                 Mat frame = new Mat();
-                frame = capture.QueryFrame();
+                capture.Read(frame);
                 #endregion
 
-                if (!frame.IsEmpty)
+                if (!frame.IsDisposed)
                 {
                     #region Detect markers on last retrieved frame
-                    VectorOfInt ids = new VectorOfInt(); // name/id of the detected markers
-                    VectorOfVectorOfPointF corners = new VectorOfVectorOfPointF(); // corners of the detected marker
-                    VectorOfVectorOfPointF rejected = new VectorOfVectorOfPointF(); // rejected contours
-                    ArucoInvoke.DetectMarkers(frame, ArucoDict, corners, ids, ArucoParameters, rejected);
+                    int[] ids= new int[0]; ; // name/id of the detected markers
+                    Point2f corners = new Point2f(); // corners of the detected marker
+                    Point2f rejected = new Point2f(); // rejected contours
+                    CvAruco.DetectMarkers(frame, ArucoDict, corners, ids, ArucoParameters, rejected);
                     #endregion
 
                     // If we detected at least one marker
-                    if (ids.Size > 0)
+                    if (ids.Length > 0)
                     {
 
                         #region Draw detected markers
-                        ArucoInvoke.DrawDetectedMarkers(frame, corners, ids, new MCvScalar(255, 0, 255));
+                        CvAruco.DrawDetectedMarkers(frame, corners, ids, new Scalar(255, 0, 255));
                         #endregion
 
 
                         #region Estimate pose for each marker using camera calibration matrix and distortion coefficents
                         Mat rvecs = new Mat(); // rotation vector
                         Mat tvecs = new Mat(); // translation vector
-                        ArucoInvoke.EstimatePoseSingleMarkers(corners, 80, cameraMatrix, distortionMatrix, rvecs, tvecs);
+                        CvAruco.EstimatePoseSingleMarkers(corners, 80, cameraMatrix, distortionMatrix, rvecs, tvecs);
                         #endregion
 
                         #region Draw 3D orthogonal axis on markers using estimated pose
@@ -133,8 +149,8 @@ namespace ArTracking
                                 rvecMat.CopyTo(values);
                                 rvec.Push(values);
                                 tvecMat.CopyTo(values);
-                                tvec.Push(values);
-                                ArucoInvoke.DrawAxis(frame,
+                                tvec.(values);
+                                Cv2.DrawFrameAxes(frame,
                                                      cameraMatrix,
                                                      distortionMatrix,
                                                      rvec,
@@ -142,7 +158,7 @@ namespace ArTracking
                                                      80 * 0.5f);
 
                                 #region Guarda os valores de translacao
-                                Dados["translation_x"] = tvec[0].ToString();
+                                Dados["translation_x"] = tvec..ToString();
                                 Dados["translation_y"] = tvec[1].ToString();
                                 Dados["translation_z"] = tvec[2].ToString();
                                 #endregion
@@ -150,11 +166,6 @@ namespace ArTracking
                         }
                         #endregion
 
-                        #region Calculo para distancia do cubo
-                        Mat rot_mtx = Mat.Zeros(3, 3, DepthType.Default, 1);
-                        //CvInvoke.Rodrigues(rvecs,rot_mtx);
-
-                        #endregion
                     }
 
 
@@ -168,12 +179,12 @@ namespace ArTracking
         public void MostraVideos(Mat frame)
         {
             #region Mostra da tela de tracking
-            CvInvoke.NamedWindow("Tracking", NamedWindowType.Normal);
-            CvInvoke.PutText(frame, ("translation_x: " + Dados["translation_x"]), new Point(0, 60), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 2, LineType.Filled);
-            CvInvoke.PutText(frame, ("translation_y: " + Dados["translation_y"]), new Point(0, 80), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 2, LineType.Filled);
-            CvInvoke.PutText(frame, ("translation_z: " + Dados["translation_z"]), new Point(0, 100), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 2, LineType.Filled);
-            CvInvoke.Imshow("Tracking", frame);
-            CvInvoke.WaitKey(24);
+            Cv2.NamedWindow("Tracking", WindowFlags.Normal);
+            Cv2.PutText(frame, ("translation_x: " + Dados["translation_x"]), new OpenCvSharp.Point(0, 60), OpenCvSharp.HersheyFonts.HersheySimplex, 0.5, new Scalar(0, 255, 0), 2, LineTypes.AntiAlias);
+            Cv2.PutText(frame, ("translation_y: " + Dados["translation_y"]), new OpenCvSharp.Point(0, 80), OpenCvSharp.HersheyFonts.HersheySimplex, 0.5, new Scalar(0, 255, 0), 2, LineTypes.AntiAlias);
+            Cv2.PutText(frame, ("translation_z: " + Dados["translation_z"]), new OpenCvSharp.Point(0, 100), OpenCvSharp.HersheyFonts.HersheySimplex, 0.5, new Scalar(0, 255, 0), 2, LineTypes.AntiAlias);
+            Cv2.ImShow("Tracking", frame);
+            Cv2.WaitKey(24);
     
 
             #endregion
@@ -185,14 +196,14 @@ namespace ArTracking
             #region Initialize Camera calibration matrix with distortion coefficients 
             // Calibration done with https://docs.opencv.org/3.4.3/d7/d21/tutorial_interactive_calibration.html
             String cameraConfigurationFile = "C:/Users/Interlab.INTERLAB-XPS/Documents/flavio/ARemC/ArTracking/ArTracking/cameraParameters.xml";
-            FileStorage fs = new FileStorage(cameraConfigurationFile, FileStorage.Mode.Read);
-            if (!fs.IsOpened)
+            FileStorage fs = new FileStorage(cameraConfigurationFile, FileStorage.Modes.Read);
+            if (!fs.IsDisposed)
             {
                 Console.WriteLine("Could not open configuration file " + cameraConfigurationFile);
                 return;
             }   
-             cameraMatrix = new Mat(new Size(3, 3), DepthType.Cv32F, 1);
-             distortionMatrix = new Mat(1, 8, DepthType.Cv32F, 1);
+             cameraMatrix = new Mat(3,3, 1);
+             distortionMatrix = new Mat(1, 8, 1);
             fs["cameraMatrix"].ReadMat(cameraMatrix);
             fs["dist_coeffs"].ReadMat(distortionMatrix);
             #endregion
